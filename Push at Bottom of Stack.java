b065f01fae78088ce72bfb860daa231b8ebf5fba
Suvin Singh <suvinsingh2003@gmail.com>
2024-08-13 11:04:12 +0530

    Create Push at Bottom of Stack.java

---
+++ Push at Bottom of Stack.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class Main{
+    
+    
+    public static void pushBottom(Stack<Integer> s, int data){
+        //base case
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        
+        //recurtion
+        
+        int top = s.pop();
+        pushBottom(s,data);
+        s.push(top);
+    }
+    
+    
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushBottom(s,4);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
