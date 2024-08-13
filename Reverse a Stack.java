757b1e0c163003216bc0acc948b6c9bcff5116af
Suvin Singh <suvinsingh2003@gmail.com>
2024-08-13 18:09:33 +0530

    Create Reverse a Stack.java

---
+++ Reverse a Stack.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class Main{
+    
+    
+    public static void pushBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        
+        int top = s.pop();
+        pushBottom(s,data);
+        s.push(top);
+    }
+    
+    public static void rev(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        rev(s);
+        pushBottom(s,top);
+    }
+    
+    public static void print(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+    
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        rev(s);
+        print(s);
+    }
+}
