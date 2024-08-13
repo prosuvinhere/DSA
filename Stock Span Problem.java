c0ec8ec13a8d9277260615838e074a1abc3fd5ce
Suvin Singh <suvinsingh2003@gmail.com>
2024-08-13 18:58:00 +0530

    Create Stock Span Problem.java

---
+++ Stock Span Problem.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void stockspan(int stock[],int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        
+        for(int  i = 1; i < stock.length ; i++){
+            int curr = stock[i];
+            while(!s.isEmpty() && curr > stock[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }else{
+                span[i] = i - s.peek();
+            }
+            s.push(i);
+        }
+    }
+    
+    
+    public static void main(String[] args){
+        int stock[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stock.length];
+        stockspan(stock,span);
+        
+        for(int i = 0; i < stock.length; i++){
+            System.out.println(span[i]);
+        }
+    }
+}
