ef9cb2f2a8537f1d5f44a629796760297efebab8
Suvin Singh <suvinsingh2003@gmail.com>
2024-08-20 12:35:09 +0530

    Update Job Sequencing Problem.java

--- Greedy Algorithm/Job Sequencing Problem.java
+++ Greedy Algorithm/Job Sequencing Problem.java
@@ -1,43 +1,35 @@
 import java.util.*;
 
-
-public class Main {
-  static class Job{
-    int deadline;
+public class Main{
+  public static class Jobclass{
+    int index;
     int profit;
-    int id;
-
-    public Job(int i,int d,int p){
-      id = i;
-      deadline = d;
+    int deadline;
+    public Jobclass(int i,int p,int d){
+      index = i;
       profit = p;
+      deadline= d;
     }
   }
   
   public static void main(String[] args){
-    int jobsInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
+    int[][] given = {{4,20},{1,10},{1,40},{1,30}};
+    
+    ArrayList<Jobclass> jobArr = new ArrayList<>();
 
-  ArrayList<Job> jobs = new ArrayList<>();
-
-    for(int i = 0 ;i < jobsInfo.length; i++){
-       jobs.add(new Job(i,jobsInfo[i][0],jobsInfo[i][1]));
+    for(int i = 0; i < given.length; i++){
+      jobArr.add(new Jobclass(i,given[i][1],given[i][0]));
     }
-  Collections.sort(jobs,(obj1,obj2) -> obj2.profit - obj1.profit); //dsecanding;
 
-
-  ArrayList<Integer> seq = new ArrayList<>();
-  int time = 0;
-  for(int i = 0; i < jobs.size(); i++){
-    Job curr = jobs.get(i);
-    if(curr.deadline > time){
-      seq.add(curr.id);
-      time++;
+    Collections.sort(jobArr,(a,b) -> b.profit - a.profit);
+    int time =0;
+    for(int i = 0;i<jobArr.size();i++){
+      Jobclass curr = jobArr.get(i);
+      if(curr.deadline > time){
+        System.out.println(curr.index);
+        time++;
+      }
     }
-  }
-  System.out.println(seq.size());
-  for(int i = 0; i<seq.size(); i++){
-    System.out.println(seq.get(i));
-  }
-  System.out.println("");
+    System.out.println(time);
   }
 }
